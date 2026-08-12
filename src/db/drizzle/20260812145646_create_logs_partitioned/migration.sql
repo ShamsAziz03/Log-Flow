3474ee6dc56e87db8d6da0f1d58302136d0489fb
ShamsAziz03 <shamsimad550@gmail.com>
2026-08-12 18:03:31 +0300

    feat: add default table for partitons in SQL migration file

--- src/db/drizzle/20260812145646_create_logs_partitioned/migration.sql
+++ src/db/drizzle/20260812145646_create_logs_partitioned/migration.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS logs CASCADE;
 DROP TYPE IF EXISTS log_level;
 CREATE TYPE log_level AS ENUM ('debug', 'info', 'warn', 'error');
 
-CREATE TABLE "logs" (
+CREATE TABLE IF NOT EXISTS "logs" (
   "id" uuid NOT NULL,
   "timestamp" timestamptz NOT NULL,
   "service" text NOT NULL,
@@ -16,3 +16,5 @@ CREATE TABLE "logs" (
 CREATE INDEX idx_logs_time_id ON logs (timestamp DESC, id DESC);
 CREATE INDEX idx_logs_service_level ON logs (service, level);
 CREATE INDEX idx_logs_attributes ON logs USING GIN (attributes jsonb_path_ops);
+
+CREATE TABLE IF NOT EXISTS logs_default PARTITION OF logs DEFAULT;
